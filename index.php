8d16d6e56fdf0330e7371155da1af1ef7a810fe8
K the builder <155670530+Keirthegreat@users.noreply.github.com>
2024-11-08 18:01:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+
+
 session_start();
 include 'db.php'; // Include your database connection
 
