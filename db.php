f05fb3f78014cdf806f9e5a88c47dc937cd505fb
K the builder <155670530+Keirthegreat@users.noreply.github.com>
2024-12-02 23:48:05 +0800

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
 
 // Connection URI from Supabase
-$uri = 'postgresql://postgres.dsoafkhbxwxhzvgivbxh:[Keirsteph@12]@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres';
+$uri = 'postgresql://postgres.dsoafkhbxwxhzvgivbxh:Keirsteph@12@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres';
 
 try {
     // Parse the URI into components
